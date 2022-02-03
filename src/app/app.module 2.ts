3c05e32781839e855cca2555c6bcfb44c3385fbd
AlexsonSilva <21643@student.dorset-college.ie>
2022-02-03 08:58:53 +0000

    initial commit

---
+++ src/app/app.module 2.ts
@@ -0,0 +1,57 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MenuComponent } from './pages/menu/menu.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MenupageComponent } from './pages/menupage/menupage.component';
+import { environment } from 'src/environments/environment';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire/compat';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { OrderDetailsService } from './services/order-details.service';
+import { EventsComponent } from './pages/events/events.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    AboutComponent,
+    MenupageComponent,
+    NavbarComponent,
+    FooterComponent,
+    SignupComponent,
+    LoginComponent,
+    EventsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [OrderDetailsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment 2.ts
@@ -0,0 +1,29 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDW-KQ5zTdgKuKLELePu6uQOVNQkGHem98",
+    authDomain: "sharingcare-19c7f.firebaseapp.com",
+    databaseURL: "https://sharingcare-19c7f-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "sharingcare-19c7f",
+    storageBucket: "sharingcare-19c7f.appspot.com",
+    messagingSenderId: "1087193945131",
+    appId: "1:1087193945131:web:ac08696b2ff3397996866e",
+    measurementId: "G-QPFXXWZZJT"
+  }
+};
+
+// export const environment = {
+//   production: false
+// };
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
